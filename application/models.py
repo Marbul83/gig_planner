b073030853839ad32d9f23f624b25ff0fb5178b3
Marbul83 <markbulfin@gmail.com>
2020-03-19 16:49:39 +0000

    fixed database

---
+++ application/models.py
@@ -0,0 +1,35 @@
+from application import db
+
+
+
+gigs = db.Table('gigs',
+    db.Column('band_id', db.Integer, db.ForeignKey('Bands.band_id')),
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venues.venue_id'))
+    )
+
+class Bands(db.Model):
+    band_id = db.Column(db.Integer, primary_key=True)
+    band_name = db.Column(db.String(60), nullable=False, unique=True)
+
+   gigplanner = db.relationships('Venues', secondary=gigs, backref=db.backref('gigplanner', lazy= True))
+   
+   venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+
+    def __repr__(self):
+        return ''.join([
+            'Band: ', self.band_name, '\r\n',
+        ])
+
+
+class Venues(db.Model):
+    venue_id = db.Column(db.Integer, primary_key=True)
+    venue_name = db.Column(db.String(30), nullable=False, unique=True)
+    
+    band_id = db.Column(db.Integer, db.ForeignKey('bands.id'), nullable=False)
+
+    def __repr__(self):
+        return ''.join(['Venue: ', self.venue_name, '\r\n',
+        ])
+
+
+# need to create composite db
