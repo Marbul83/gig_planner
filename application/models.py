1e71ef4389bd3b6e00173d17a2c8020bd046be0d
Marbul83 <markbulfin@gmail.com>
2020-03-19 16:45:26 +0000

    added database

--- application/models.py
+++ application/models.py
@@ -1,11 +1,19 @@
 from application import db
 
 
+
+gigs = db.Table('gigs',
+    db.Column('band_id', db.Integer, db.ForeignKey('Bands.band_id')),
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venues.venue_id'))
+    )
+
 class Bands(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    band_id = db.Column(db.Integer, primary_key=True)
     band_name = db.Column(db.String(60), nullable=False, unique=True)
 
-    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+   gigplanner = db.relationships('Venues', secondary=gigs, backref=db.backref('gigplanner', lazy= True))
+   
+   venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
 
     def __repr__(self):
         return ''.join([
@@ -14,7 +22,7 @@ class Bands(db.Model):
 
 
 class Venues(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, primary_key=True)
     venue_name = db.Column(db.String(30), nullable=False, unique=True)
     
     band_id = db.Column(db.Integer, db.ForeignKey('bands.id'), nullable=False)
